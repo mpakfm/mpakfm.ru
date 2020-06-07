a7f1ab7bbdd2f7164d33dbf2aaa8a701131696ba
Sergey Fomin <mpakfm@gmail.com>
2020-06-08 01:26:56 +0300

    Index controller

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Mpakfm\Printu;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test()
+    {
+
+        Printu::log('test', 'Index');
+        $response = new Response();
+        return $response;
+    }
+}
